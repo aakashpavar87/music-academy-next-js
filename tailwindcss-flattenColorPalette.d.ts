5cac6515281f8ea0ac2de8be790dbe54e071cca1
Aakash Pavar <aakashpavar87@gmail.com>
2024-05-27 17:32:22 +0530

    added tailwind ts file for flattencolorpallete

---
+++ tailwindcss-flattenColorPalette.d.ts
@@ -0,0 +1,6 @@
+declare module 'tailwindcss/lib/util/flattenColorPalette' {
+    // You can specify the actual types if you know them or leave it as 'any' for now.
+    const flattenColorPalette: any;
+    export default flattenColorPalette;
+  }
+  
